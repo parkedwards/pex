8dd96eb9276ead8899917479320af61591a5922a
EDWARD PARK <ed.sh.park@gmail.com>
2017-07-31 01:03:50 -0700

    adding dotenv to development environment + solidifying API main entries

--- server/api.js
+++ server/api.js
@@ -1,18 +1,21 @@
 const { Router } = require('express');
+const { encryptPassword } = require('./utils/bcrypt');
+
+const authRoutes = require('./auth/routes');
+const userRoutes = require('./user/routes');
 
 const api = new Router();
 
-api.get('/', () => console.log('ayyyyyyyyy'));
+api.get('/', () => {
+  console.log('ayyyyyyyyy');
+  const email = 'esangpark@gmail.com';
+  const password = 'Shortybord-45';
+  console.log(encryptPassword(password));
+});
+
 api.get('/test', () => console.log('testing testing bitches'));
 
-api.post('/auth', (req, res) => {
-  console.log('<< POST : login >>');
-  return res
-    .status(201)
-    .json(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMzQ1Njc4OTAiLCJuYW1lIjoiSm9obiBEb2UiLCJ1c2VybmFtZSI6ImVkd2FyZEBncmF2aXR5YnJhbmRzLmNvbSJ9.PFR0HeeVvpHiDeg2B56ucE-WRkhQU4CmgWu-x73bDo0',
-    );
-});
-api.post('/auth/create', () => console.log('<< POST : sign up >>'));
+api.use('/auth', authRoutes);
+api.use('/user', userRoutes);
 
 module.exports = api;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV === 'dev') {
+  const dotenv = require('dotenv').config();
+}
 
+const PORT = process.env.PORT || 3000;
 const api = require('./api');
 
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }), bodyParser.json());
+app.use('*', cors()); // enable pre-flight CORS
 
 // pulls from the /dist folder, meaning you don't have to explicitly server the '/' route
 app.use(express.static(path.join(__dirname, '../dist')));
