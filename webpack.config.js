281e1a5687020e45ca09d6bf56c5a6c55be0dbfa
EDWARD PARK <ed.sh.park@gmail.com>
2017-07-30 19:40:36 -0700

    adding a -watch flag to build script; adding jwt dependencies for creation + decoding

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: ["./client/src/index.jsx"],
+  entry: ['./client/src/index.jsx'],
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "bundle.js"
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         query: {
-          presets: ["react", "es2015", "stage-1"]
-        }
-      }
-    ]
+          presets: ['react', 'es2015', 'stage-1'],
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
+    extensions: ['*', '.js', '.jsx'],
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: path.join(__dirname, "dist")
-  }
+    contentBase: path.join(__dirname, 'dist'),
+  },
 };
