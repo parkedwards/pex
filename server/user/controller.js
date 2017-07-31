db73ab80fcf092d0903bff27a56c8ef71e68a310
EDWARD PARK <ed.sh.park@gmail.com>
2017-07-31 01:02:04 -0700

    building out user service on API to house + process user detail data

---
+++ server/user/controller.js
@@ -0,0 +1,26 @@
+const db = require('./model');
+
+module.exports = {
+  fetchUser: (req, res) => {
+    db.query(
+      'select * from users where userid=$1 limit 1',
+      [req.body.userId],
+      (err, user) => {
+        if (err) {
+          console.error(err);
+          return res
+            .status(401)
+            .json('Something went wrong with your request.');
+        }
+        if (!user.rows[0]) {
+          return res.status(402).json('User retrieval failed.  Check query again');
+        }
+
+        return res.status(201).json({
+          first_name: user.rows[0].first_name,
+          last_name: user.rows[0].last_name,
+        });
+      },
+    );
+  },
+};
---
+++ server/user/model.js
@@ -0,0 +1,19 @@
+const db = require('../database');
+
+db.query(
+  `create table if not exists "users" (
+  _id serial primary key,
+  userId integer unique not null,
+  email varchar(256) unique not null,
+  first_name varchar(256) not null,
+  last_name varchar(256) not null
+)`,
+  err => {
+    if (err) {
+      console.error('===== issue with user table creation ======');
+      console.error(err);
+    }
+  },
+);
+
+module.exports = db;
