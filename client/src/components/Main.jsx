bb3ff9a17a674a403563f88f17d2302c8c305b84
EDWARD PARK <ed.sh.park@gmail.com>
2017-08-05 22:49:08 -0700

    creating a <Greeting> flow when authenticated -- still need to plan out some wireframes before doing any more UI work

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
-import { style } from '../utils';
+import { style, constants } from '../utils';
 
+const { hello } = constants;
 const { flex, media } = style;
 
 class Main extends Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'hello',
+    };
+  }
+  componentWillMount() {
     // make user info request here using the user Id + JWT in header!
     const { requestUser, user } = this.props;
     requestUser(user.userId);
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        view: 'weight',
+      });
+    }, 2500);
+  }
+
   render() {
+    const { view } = this.state;
+
     if (!this.props.user.first_name) {
       return null;
     }
     return (
       <Wrapper>
-        Hola, <Emphasis>{this.props.user.first_name}!</Emphasis>
+        {view === 'hello' &&
+          <Greeting>
+            {hello[Math.floor(Math.random() * hello.length)]}
+            ,
+            {' '}
+            <Emphasis>{this.props.user.first_name}!</Emphasis>
+          </Greeting>}
+
+        {view === 'weight' &&
+          <Greeting>{'Please input your weight now:'}</Greeting>}
       </Wrapper>
     );
   }
@@ -38,6 +64,9 @@ const fadeInFromTop = keyframes`
 
 const Wrapper = styled.div`
   font-size: 1.5em;
+`;
+
+const Greeting = styled.div`
   animation: 1s ${fadeInFromTop} ease;
 `;
 
